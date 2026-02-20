50b412bdd168e1867e4b594d49578ce9b3f22b64
copperdevs <github@copperdevs.com>
2026-02-20 14:08:33 -0600

    mirror vector extensions on int vector

--- Celesium/Utility/VectorExtensions.cs
+++ Celesium/Utility/VectorExtensions.cs
@@ -50,4 +50,51 @@ public static class VectorExtensions
         public Vector4 InvertZ() => vector.WithZ(-vector.Z);
         public Vector4 InvertW() => vector.WithW(-vector.W);
     }
+
+    extension(Vector2Int vector)
+    {
+        public Vector2Int WithX(int x) => vector with { X = x };
+        public Vector2Int WithY(int y) => vector with { Y = y };
+
+        public Vector2Int InvertX() => vector.WithX(-vector.X);
+        public Vector2Int InvertY() => vector.WithY(-vector.Y);
+
+        public Vector3Int ToVector3(int z = 0) => new(vector.X, vector.Y, z);
+        public Vector4Int ToVector4(int z = 0, int w = 0) => new(vector.X, vector.Y, z, w);
+    }
+
+    extension(Vector3Int vector)
+    {
+        public Vector3Int WithX(int x) => vector with { X = x };
+        public Vector3Int WithY(int y) => vector with { Y = y };
+        public Vector3Int WithZ(int z) => vector with { Z = z };
+
+        public Vector2Int WithoutX() => new(vector.Y, vector.Z);
+        public Vector2Int WithoutY() => new(vector.X, vector.Z);
+        public Vector2Int WithoutZ() => new(vector.X, vector.Y);
+
+        public Vector3Int InvertX() => vector.WithX(-vector.X);
+        public Vector3Int InvertY() => vector.WithY(-vector.Y);
+        public Vector3Int InvertZ() => vector.WithZ(-vector.Z);
+
+        public Vector4Int ToVector4(int w = 0) => new(vector.X, vector.Y, vector.Z, w);
+    }
+
+    extension(Vector4Int vector)
+    {
+        public Vector4Int WithX(int x) => vector with { X = x };
+        public Vector4Int WithY(int y) => vector with { Y = y };
+        public Vector4Int WithZ(int z) => vector with { Z = z };
+        public Vector4Int WithW(int w) => vector with { W = w };
+
+        public Vector3Int WithoutX() => new(vector.Y, vector.Z, vector.W);
+        public Vector3Int WithoutY() => new(vector.X, vector.Z, vector.W);
+        public Vector3Int WithoutZ() => new(vector.X, vector.Y, vector.W);
+        public Vector3Int WithoutW() => new(vector.X, vector.Y, vector.Z);
+
+        public Vector4Int InvertX() => vector.WithX(-vector.X);
+        public Vector4Int InvertY() => vector.WithY(-vector.Y);
+        public Vector4Int InvertZ() => vector.WithZ(-vector.Z);
+        public Vector4Int InvertW() => vector.WithW(-vector.W);
+    }
 }
